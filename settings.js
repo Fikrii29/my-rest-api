0da484dc2216cb80acd00d432431dc3727695b34
Fikrii29 <148403723+Fikrii29@users.noreply.github.com>
2023-10-27 04:16:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://fikriisky.szfjlzu.mongodb.net/" --apiVersion 1 --username fikriisekay9" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "fikriisekai9@gmail.com" //email
+global.email_password = "cwag owlo giaj qoxx" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
